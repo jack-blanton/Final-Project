2cc3b8f3f36a897158c9a9d7c755142847e6a6ec
Blanton <26BlantonJ@badinhs.org>
2025-05-01 11:23:25 -0400

    finished the game, I meant to do another commit during 8th bell yesterday but I scrapped most of the code from that anyways so it doesn't really matter. if anything I just need to refine it a little bit more

--- guessinggame.js
+++ guessinggame.js
@@ -1,8 +1,28 @@
 let guessAmount = 0;
 let gameAmount = 0;
-
+let guess = undefined;
+let averageGuesses;
 function guessinggame(){
     let bot = Math.round(Math.random()*100) + 1
-    
+    while(guess !== bot){
+    if(guess == undefined){
+            guess = Number(prompt("Pick a number 1-100"))
+            guessAmount = guessAmount + 1;
+            console.log(typeof(guess))}
+     else if (guess > bot){
+        guess = Number(prompt("Too large. Guess again!"))
+        guessAmount = guessAmount + 1;}
+     else{
+        guess = Number(prompt("Too small. Guess again!"))
+        guessAmount = guessAmount + 1;
+        console.log(guess)}
+    }
+gameAmount = gameAmount + 1;
+averageGuesses = guessAmount / gameAmount
+alert(`good job! the number was ${bot}`)
+alert(`Your average amount of guesses to get the number is ${averageGuesses}`)
+alert(`Game ${gameAmount + 1}`)
+guess = undefined;
+guessinggame()
 }
-guessinggame();
+guessinggame()
