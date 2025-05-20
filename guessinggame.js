22d56468b436fed6c1ce3d8f2a476ba4fb9ab1a6
Blanton <26BlantonJ@badinhs.org>
2025-05-20 14:01:22 -0400

    added amount of guesses per round

--- guessinggame.js
+++ guessinggame.js
@@ -1,25 +1,30 @@
 let guessAmount = 0;
 let gameAmount = 0;
 let guess = undefined;
-let averageGuesses;
+let averageGuesses = 0;
 function guessinggame(){
+    let tempGuess = 0;
     let bot = Math.round(Math.random()*100) + 1
     while(guess !== bot){
     if(guess == undefined){
             guess = Number(prompt("Pick a number 1-100"))
             guessAmount = guessAmount + 1;
+            tempguess = tempGuess + 1;
             console.log(typeof(guess))}
      else if (guess > bot){
         guess = Number(prompt("Too large. Guess again!"))
         guessAmount = guessAmount + 1;}
+         tempguess = tempGuess + 1;
      else{
         guess = Number(prompt("Too small. Guess again!"))
         guessAmount = guessAmount + 1;
+         tempguess = tempGuess + 1;
         console.log(guess)}
     }
 gameAmount = gameAmount + 1;
 averageGuesses = guessAmount / gameAmount
 alert(`good job! the number was ${bot}`)
+alert(`It took you ${tempGuess} guesses!`)
 alert(`Your average amount of guesses to get the number is ${averageGuesses}`)
 guess = undefined;
 if(confirm("Play again?") == true){
